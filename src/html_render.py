ad92b9557da217f360cdd0321a7a8aee132abb10
LogicAnd <79437261+LogicAnd@users.noreply.github.com>
2021-12-05 21:35:47 +0800

    sec
    sec

--- src/html_render.py
+++
@@ -1,5 +0,0 @@
-with open('data/comments_data.json', 'r',encoding='utf8') as f:
-    data = f.read()
-jsfile = 'var data = ' + data
-with open('html/data.js', 'w', encoding='utf8') as f:
-    f.write(jsfile)
